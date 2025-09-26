acffcc998d3ed0aa9d387faa661758bb6ed2a769
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-09-26 10:25:05 +0200

    chore : Create WelcomeScreen / Refactor code / Add assets directory

---
+++ code/tictic_infor/lib/screens/welcome_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SvgPicture.asset(
+              ''
+          )
+          // LOGO
+          // Carousel
+          // main Button
+          // Line
+          // ROW -> Double button
+        ],
+      ),
+    );
+  }
+}
