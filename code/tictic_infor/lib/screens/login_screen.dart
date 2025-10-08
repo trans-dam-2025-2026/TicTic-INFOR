82f5399c04f220f17fc211cab19b87f064f970ce
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-10-08 13:46:41 +0200

    fix: Fix target link

--- code/tictic_infor/lib/screens/login_screen.dart
+++ code/tictic_infor/lib/screens/login_screen.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:tictic_infor/screens/home_screen.dart';
-import 'package:tictic_infor/styles/colors.dart';
-import 'package:tictic_infor/styles/images.dart';
-import 'package:tictic_infor/styles/spacings.dart';
+import 'package:tictic_infor/screens/register_screen.dart';
 import 'package:tictic_infor/widgets/link_to_route.dart';
 import 'package:tictic_infor/widgets/login_form.dart';
 import 'package:tictic_infor/widgets/logo_svg_click.dart';
-import 'package:tictic_infor/widgets/main_button.dart';
-import 'package:tictic_infor/widgets/password_input.dart';
-import 'package:tictic_infor/widgets/text_input.dart';
 import 'package:tictic_infor/widgets/w_back_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -46,7 +39,7 @@ class _LoginScreenState extends State<LoginScreen> {
               LinkToRoute(
                 text: 'Je n’ai pas encore de compte.',
                 label: 'Créer mon compte !',
-                routeName: LoginScreen.routeName,
+                routeName: RegisterScreen.routeName,
               ),
             ],
           ),
