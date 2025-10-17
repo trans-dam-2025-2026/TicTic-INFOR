b8b725f8f33931963fdb485fc9c30a3d0f497b2a
Dylan Jacquet <dylanjacquet235@gmail.com>
2025-10-17 08:57:36 +0200

    chore : init Internationalizing

--- code/tictic_infor/lib/main.dart
+++ code/tictic_infor/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tictic_infor/router/router.dart';
 import 'package:tictic_infor/screens/welcome_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TicTic',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('fr'), // Français
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
